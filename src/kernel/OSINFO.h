40a045ed294e7506689f15015c8957d6ec15c15a
Qunerum <qunerumofficial@gmail.com>
2026-08-09 16:35:21 +0200

    update 2026-08-09 16:35

--- src/kernel/OSINFO.h
+++
@@ -1,6 +0,0 @@
-#ifndef OSINFO_H
-#define OSINFO_H
-
-#define OS_VERSION "0.1.0"
-
-#endif
--- src/lib/math.h
+++
@@ -1,7 +0,0 @@
-#ifndef MATH_H
-#define MATH_H
-
-void get_circle_coords(int angle_deg, int* out_x, int* out_y);
-int absolute(int v);
-
-#endif
